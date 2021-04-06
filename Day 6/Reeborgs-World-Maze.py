97d1cc9984b9c310f5055036f44f293bbe8634f0
alozieoge <39676713+alozieoge@users.noreply.github.com>
2021-04-06 20:31:50 +0100

    Create Reeborgs-World-Maze.py
    Maze exercise in Reeborg's world.

---
+++ Day 6/Reeborgs-World-Maze.py
@@ -0,0 +1,37 @@
+# 
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+# Solution 1
+# Set a limit on the number of right turns
+right_turns = 0
+while not at_goal():
+    if right_is_clear() and right_turns < 3:
+        turn_right()
+        right_turns += 1
+        move()
+    elif front_is_clear():
+        move()
+        right_turns = 0
+    else:
+        turn_left()
+        right_turns = 0
+    # print(right_turns)
+    
+# Solution 2
+# Make sure the robot has a wall on its right side
+while front_is_clear():
+    move()
+turn_left()
+
+while not at_goal():
+    if right_is_clear():
+        turn_right()
+        move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
