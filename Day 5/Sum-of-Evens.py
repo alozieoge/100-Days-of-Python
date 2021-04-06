24354e2c2044326a416702f54aae6694f977a9cb
alozieoge <39676713+alozieoge@users.noreply.github.com>
2021-04-06 05:18:58 +0100

    Create Sum-of-Evens.py
    Calculates the sum of even numbers between 1 and 100

---
+++ Day 5/Sum-of-Evens.py
@@ -0,0 +1,10 @@
+total_even = 0
+
+for number in range(2, 101, 2):
+  total_even += number
+print(total_even)
+
+# for number in range(1, 101):
+#   if number % 2 == 0:
+#     total_even += number
+# print(total_even)
