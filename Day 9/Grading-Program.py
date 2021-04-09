93886f5ad64303fbfe3a7fb6feeb781a39d84b65
alozieoge <39676713+alozieoge@users.noreply.github.com>
2021-04-09 21:12:36 +0100

    Create Grading-Program.py
    Grading students based on their scores.

---
+++ Day 9/Grading-Program.py
@@ -0,0 +1,29 @@
+student_scores = {
+  "Harry": 81,
+  "Ron": 78,
+  "Hermione": 99, 
+  "Draco": 74,
+  "Neville": 62,
+}
+
+# Create an empty dictionary called student_grades.
+student_grades = {}
+
+# Add the grades to student_grades.
+for student in student_scores:
+    score = student_scores[student]
+    if score > 90:
+        student_grades[student] = "Outstanding"
+    elif score > 80:
+        student_grades[student] = "Exceeds Expectation"
+    elif score > 70:
+        student_grades[student] = "Acceptable"
+    else:
+        student_grades[student] = "Fail"
+
+print(student_grades)
+
+
+
+
+
