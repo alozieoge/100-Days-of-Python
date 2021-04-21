804929d8b6843651421024fb6c03a7a2363eab3c
alozieoge <39676713+alozieoge@users.noreply.github.com>
2021-04-21 19:32:17 +0100

    Update main.py
    Update the main file with food and wall collision logic.

--- Day 20/main.py
+++ Day 20/main.py
@@ -23,8 +23,28 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
+    
+    # Detect collision with food using turtle.distance()
+    # Which returns the distance between two turtles.
+    if snake.head.distance(food) < 15:  # If the snake head is within 15 pixels of the food or closer
+        # print("nom nom nom")
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    # Detect collision with wall (boundary)
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    # Detect collision with tail
+    for segment in snake.segments[1:]:
+        # If head collides any other segment in the tail:
+        # trigger game over sequence
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
