a0599192b8871783f8a44375583a8e67a22e51a5
alozieoge <39676713+alozieoge@users.noreply.github.com>
2021-05-31 13:33:34 +0100

    Update main.py

--- Day 61/flask-wtform-bootstrap-secret/main.py
+++ Day 61/flask-wtform-bootstrap-secret/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template #, url_for, redirect
+from flask import Flask, render_template
 from login_form import LoginForm
 from flask_bootstrap import Bootstrap
 
@@ -20,23 +20,13 @@ def login():
     login_form = LoginForm()
     # login_form.validate_on_submit() # This line forces the login form to be submitted (POST request) irrespective of
     # whether a page loading (GET request) is intended.
-    # if request.method == 'POST':
-    #     return render_template('login.html')
     if login_form.validate_on_submit():
         # print(login_form.email.data, login_form.password.data)
         if login_form.email.data == admin_email and int(login_form.password.data) == admin_password:
             return render_template('success.html')
         else:
             return render_template('denied.html')
-    # else:
-    #     print("Loading login.html")
     return render_template('login.html', form=login_form)
-
-
-@app.route('/test')
-def test_page():
-    print("Test")
-    return render_template('success.html')
 
 
 if __name__ == '__main__':
